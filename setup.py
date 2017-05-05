5863bed9a92667e443314df344af1d11572a6345
William Tran <viet.tran@outlook.com>
2017-05-05 16:22:42 -0400

    chore(semver): increase patch version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from os.path import join, dirname
 from setuptools import setup, find_packages
 
-VERSION = (5, 0, 0)
+VERSION = (5, 0, 1)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
